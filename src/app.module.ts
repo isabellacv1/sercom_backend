616853811661e20924318b2740e6939c09d4b26c
ValenTobar <valentinatobar05@gmail.com>
2026-05-15 11:38:02 -0500

    feat: app module updated with a new certifications module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ChatModule } from './chat/chat.module';
 import { WorkerProfileModule } from './worker-profile/worker-profile.module';
 import { PaymentsModule } from './payments/payments.module';
 import { ChatbotModule } from './chatbot/chatbot.module';
+import { CertificationsModule } from './certifications/certifications.module';
 
 @Module({
   imports: [
@@ -32,6 +33,7 @@ import { ChatbotModule } from './chatbot/chatbot.module';
     WorkerProfileModule,
     PaymentsModule,
     ChatbotModule,
+    CertificationsModule,
   ],
   providers: [ServiceOptionsService],
   controllers: [ServiceOptionsController],
